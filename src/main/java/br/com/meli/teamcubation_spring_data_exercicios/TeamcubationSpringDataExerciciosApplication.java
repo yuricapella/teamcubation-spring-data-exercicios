5ce87303d7e725a8b3393839dee9a6f823d99479
yuricapella <yuricapelladossantos@gmail.com>
2025-07-03 16:42:13 -0300

    adicionando projeto ao repositorio remoto

---
+++ src/main/java/br/com/meli/teamcubation_spring_data_exercicios/TeamcubationSpringDataExerciciosApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.teamcubation_spring_data_exercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcubationSpringDataExerciciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcubationSpringDataExerciciosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/teamcubation_spring_data_exercicios/TeamcubationSpringDataExerciciosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.teamcubation_spring_data_exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcubationSpringDataExerciciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
