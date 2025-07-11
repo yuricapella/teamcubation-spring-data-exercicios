4fe8b852f2d05722269365070b53d5bcb3409831
yuricapella <yuricapelladossantos@gmail.com>
2025-07-11 15:12:35 -0300

    feat(exercicio02): adiciona exceção personalizada que estende RuntimeException
    - Cria exceção específica extendendo RuntimeException
    - Lança a exceção no método da classe Exercicio02

---
+++ src/main/java/br/com/meli/teamcubation_spring_data_exercicios/exercicios/Exercicio02.java
@@ -0,0 +1,8 @@
+package br.com.meli.teamcubation_spring_data_exercicios.exercicios;
+
+public class Exercicio02 {
+
+    public void lancarExcecao(){
+        throw new Exercicio02Exception("Exceção lançada!");
+    }
+}
